782fc34d7eed7871c95f0da6b1184902d9f66655
lotto-ilbo <minjeong092798@gmail.com>
2026-08-05 22:52:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 로또일보 분석조합기 폰버전 v66 — 항상 최신 코드 우선 */
-const CACHE = 'lottoilbo-m-v66';
+const CACHE = 'lottoilbo-m-v68';
 self.addEventListener('install', (e) => { self.skipWaiting(); });
 self.addEventListener('activate', (e) => {
   e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
